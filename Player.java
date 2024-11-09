37838c1fa8c9dc1aeae161d9bb4a6a31024389a6
sudoweilder <robertec2007@gmail.com>
2024-11-08 21:05:03 -0800

    Enemy file for dungeon three. Can be used by others.

--- Player.java
+++ Player.java
@@ -1,28 +1,39 @@
 import java.util.Random;
-import java.util.HashMap;
 
 
 public class Player{
     
+    String name;
     int health = 100; //will update file to so that we can have an interface that directly interacts with these variables.
     int savePoint = 0;
+    int burn = 0;
+    int poison = 0;
+    int peaShooterAmmo = 20;
+    
+    
     
     public static void main(String[] args){
 	Random random = new Random();
 	System.out.print(peaShooter(100, "chicken", 5, random));
     }
-    
-    public static int peaShooter( String targetName, int targetHealth, int rounds, Random random){ 
+
+
+    // weapons programs without objects 
+    public static int peaShooter( String targetName, int targetHealth, Random random){ 
 	//initializes variables needed for the code to work
 	int damage = 45;
 	int hitNumber = random.nextInt(100);
-
-	if (hitNumber < 50){
-	    System.out.println("You dealt " + damage + " damage to the " + targetName);
-	    targetHealth = targetHealth - damage;
-	} else System.out.println("You missed.");
 	
-	return targetHealth;//We need to set this up to return the rounds and health or have rounds processed outside the function.
+	if peaShooterAmmo > 0{
+	    if (hitNumber < 50){
+		System.out.println("You dealt " + damage + " damage to the " + targetName);
+		targetHealth = targetHealth - damage;
+	    } else System.out.println("You missed.");
+
+	    peaShooterAmmo -= 1;
+	} else System.out.println("You're out of ammo")
+	
+	return targetHealth;
 	
     }
     
@@ -34,6 +45,22 @@ public class Player{
 	int damage = 35;
 	System.out.println("You dealt " + damage + " damage to the " + targetName);
 	return targetHealth - damage;
+    }
+
+    // Object-oriented methods(for whoever wants to use them.)
+    public static void peaShooter(Enemy target, Random random){
+    	int damage = 45;
+	int hitNumber = random.nextInt(100);
+	
+	if peaShooterAmmo > 0{
+	    if (hitNumber < 50){
+		System.out.println("You dealt " + damage + " damage to the " + target.name);
+		target.health = target.health - damage;
+	    } else System.out.println("You missed.");
+
+	    peaShooterAmmo -= 1;
+	} else System.out.println("You're out of ammunition")//possibly add this to the player attack function.
+
     }
     
 }
