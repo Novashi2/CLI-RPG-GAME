14b950d269e0a1840dbb7f5207b430607a535c54
sudoweilder <robertec2007@gmail.com>
2024-11-15 16:15:31 -0800

    Battle function version 1

--- General.java
+++ General.java
@@ -1,11 +1,35 @@
 import java.util.Scanner;
-
+import java.util.Random;
 
 public class General {
     public static void main(String[] args) {
-        
+	Player player = new Player();
+	Random random = new Random();
+	Scanner console = new Scanner(System.in);
+	Enemy spider = new Enemy();
+	spider.setSpider();
+	battle(player, spider, random, console);
     }
 
+    public static void battle(Player player, Enemy enemy, Random random, Scanner console){
+	
+	while (player.health > 0 && enemy.health > 0){
+	    //player.useItem(); -- function is not programmed yet, so it is commented out at the moment
+	    player.attack(enemy, random, console);
+	    enemy.dealEffects();
+	    enemy.attack(random, player);
+	    player.dealEffects();
+	}
 
+	if (player.health <= 0){
+	    System.out.println("You have been killed by the " + enemy.name + ".");
+	    System.exit(0); // This will likely be put in a player.death() method
+	}else{
+	    System.out.println("You have slain the " + enemy.name + ".");
+	    // possible add of drops here or in the funciton where the battle occurrs in?
+	}
+    }
+
+	
     
 }
