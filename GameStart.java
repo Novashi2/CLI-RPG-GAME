81fa55abaa02ab0f8f2b20a7b1d7090d83865b38
sudoweilder <robertec2007@gmail.com>
2024-11-16 22:54:10 -0800

    added a player object constructor

--- GameStart.java
+++ GameStart.java
@@ -10,7 +10,7 @@ public class GameStart {
         //instantiates a new Random object
         Random r = new Random(); 
         
-	Player player = new Player();
+	Player player = new Player(console);
         // Health that has not been moved out yet.
         int dragonHealth = 100;
         int skeletonHealth = 30;
--- General.java
+++ General.java
@@ -1,16 +1,13 @@
 import java.util.Scanner;
 import java.util.Random;
+import java.io.FileNotFoundException;
+import java.io.File;
 
 public class General {
     public static void main(String[] args) {
-	Player player = new Player();
-	Random random = new Random();
-	Scanner console = new Scanner(System.in);
-	Enemy spider = new Enemy();
-	spider.setSpider();
-	battle(player, spider, random, console);
     }
-
+    
+    // This is the battle function for the program.
     public static void battle(Player player, Enemy enemy, Random random, Scanner console){
 	
 	while (player.health > 0 && enemy.health > 0){
@@ -22,7 +19,7 @@ public class General {
 	}
 
 	if (enemy.health <= 0) {
-	    System.out.println("You have been killed by the " + enemy.name + ".");
+	    System.out.println("You slain the " + enemy.name + ".");
 	    // Possible drop
 	}
 
@@ -33,6 +30,30 @@ public class General {
 	
     }
 
-	
-    
+    // This method takes a file name and integer. Afterwards, it uses the file name to print the entire file if the textNumber
+    // parameter is -1. If textNumber >= 0, sections of the code will be printed. Sections are determined by putting the word "break"
+    // on a line at the end of a section of text. 
+    public static void printText(String fileName, int textNumber) throws FileNotFoundException{
+	Scanner inputFile = new Scanner(new File(fileName));
+	if (textNumber == -1){
+	    while (inputFile.hasNextLine()) System.out.println(inputFile.nextLine());
+	} else {
+	    int i = 0;
+	    String text = "";
+	    System.out.print(text);
+	    // gets to text section to print
+	    while (i != textNumber && inputFile.hasNextLine()){
+		text = inputFile.nextLine();
+		if (text.startsWith("break")) i++;
+	    }
+	    //prints section--fencpost issue occurrs, so the nextLine is called before the while loop starts
+	    if (inputFile.hasNextLine()){
+		text = inputFile.nextLine();
+	    }
+	    while (!text.startsWith("break") && inputFile.hasNextLine()){
+		System.out.println(text);
+		text = inputFile.nextLine();
+	    }
+	}
+    }
 }
